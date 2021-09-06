0cb29cf9109fb9fc6289e3bdce836d61bbb6b9bc
Humberto Marcone <humberto.freitas@squadra.com.br>
2021-09-06 10:02:42 -0300

    ASP .NET Core - Consumindo uma Web API com React - lll

--- AlunosApi/Controllers/AlunosController.cs
+++ AlunosApi/Controllers/AlunosController.cs
@@ -1,5 +1,9 @@
-﻿using AlunosApi.Services;
+﻿using AlunosApi.Model;
+using AlunosApi.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AlunosApi.Controllers
 {
@@ -12,6 +16,119 @@ namespace AlunosApi.Controllers
         public AlunosController(IAlunoService alunoService)
         {
             _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunos();
+                return Ok(alunos);
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+
+        [HttpGet("AlunosPorNome")]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunosByName([FromBody] string nome)
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunosByNome(nome);
+
+                if(alunos == null)
+                    return NotFound($"Não existem alunos com o critério {nome}");
+
+                return Ok(alunos);
+            }
+            catch
+            {
+                return BadRequest("Request inválido!");
+            }
+        }
+
+        [HttpGet("{id:int}", Name = "GetAluno")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+
+                if (aluno == null)
+                    return NotFound($"Não existe aluno com o id={id}");
+
+                return Ok(aluno);
+            }
+            catch
+            {
+                return BadRequest("Request inválido!");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Aluno aluno)
+        {
+            try
+            {
+                await _alunoService.CreateAluno(aluno);
+                return CreatedAtRoute(nameof(GetAluno), new { id = aluno.Id }, aluno);
+            }
+            catch (System.Exception)
+            {
+
+                return BadRequest("Request inválido!");
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Edit(int id, [FromBody] Aluno aluno)
+        {
+            try
+            {
+                if(aluno.Id == id)
+                {
+                    await _alunoService.UpdateAluno(aluno);
+                    return Ok($"Aluno com id={id} foi atualizado com sucesso!");
+                }
+                else
+                {
+                    return BadRequest("Dados inconsistentes!");
+                }
+            }
+            catch (System.Exception)
+            {
+
+                return BadRequest("Request inválido!");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+
+                if (aluno != null)
+                {
+                    await _alunoService.DeleteAluno(aluno);
+                    return Ok($"Aluno com id={id} foi excluído com sucesso!");
+                }
+                else
+                {
+                    return NotFound($"Aluno com id={id} não encontrado");
+                }
+            }
+            catch (System.Exception)
+            {
+
+                return BadRequest("Request inválido!");
+            }
         }
     }
 }
